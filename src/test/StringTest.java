aaa7bddd5cba927c35cdc4395faddf9bd63b1f2c
Carlos Fonseca <carlosaacf@gmail.com>
2022-11-21 15:18:13 -0300

    inclusao de 2 testes de Junit

--- src/test/StringTest.java
+++ src/test/StringTest.java
@@ -2,6 +2,7 @@ package test;
 
 
 
+import static org.junit.Assert.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
@@ -9,11 +10,21 @@ import org.junit.jupiter.api.Test;
 class StringTest {
 
 	@Test
-	void test() {
+	void length_basic() {
 		int actualLength = "ABCD".length();
 		int expectedLength = 4;
 		
 		assertEquals(actualLength, expectedLength);
 		
 	}
+	
+	@Test
+	void toUpperCase_basic() {
+		String str = "abcd";
+		String result = str.toUpperCase();
+		assertEquals("ABCD", result);
+		assertNotNull(result);
+	}
 }
+
+
